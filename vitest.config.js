2f3e2010a81eb1d254046d965e091f5906bbdacf
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-09-30 13:48:32 +0300

    Upload coverage

--- vitest.config.js
+++ vitest.config.js
@@ -4,8 +4,21 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "happy-dom",
-
+    
     globals: true,
+    
+    coverage: {
+      provider: "v8",
+      
+      reporter: [
+        "text", 
+        "lcov",
+      ],
+
+      include: [
+        "src/**/*"
+      ],
+    },
   },
 
   resolve: {
